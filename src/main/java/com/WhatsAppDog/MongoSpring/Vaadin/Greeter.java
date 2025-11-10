b7b023c2fab4aaace8ae317918a6137a7c11f73d
Ying <86930915+Yl9464@users.noreply.github.com>
2025-11-10 09:00:32 -0500

    added vaadin example structure files, nonfunctional

---
+++ src/main/java/com/WhatsAppDog/MongoSpring/Vaadin/Greeter.java
@@ -0,0 +1,15 @@
+package com.WhatsAppDog.MongoSpring.Vaadin;
+
+
+import com.vaadin.flow.spring.annotation.SpringComponent;
+import com.vaadin.flow.spring.annotation.UIScope;
+
+@SpringComponent
+@UIScope
+public class Greeter {
+
+    public String sayHello() {
+        return "Hello from bean " + toString();
+    }
+
+}
