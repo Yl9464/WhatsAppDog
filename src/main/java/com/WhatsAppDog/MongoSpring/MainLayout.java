68dd5e99c13fd532e99a247c3fae2d7d3c4c40e8
Ying <86930915+Yl9464@users.noreply.github.com>
2025-11-11 17:37:29 -0500

    Buttons and app name added

--- src/main/java/com/WhatsAppDog/MongoSpring/MainLayout.java
+++ src/main/java/com/WhatsAppDog/MongoSpring/MainLayout.java
@@ -1,24 +1,21 @@
 package com.WhatsAppDog.MongoSpring;
 
-import com.WhatsAppDog.MongoSpring.Views.NavBar;
-import com.vaadin.flow.component.Composite;
-import com.vaadin.flow.component.HasComponents;
+import com.WhatsAppDog.MongoSpring.Views.Buttons;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
-import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.Route;
-import com.vaadin.flow.router.RouteAlias;
 import com.vaadin.flow.router.RouterLink;
 
 
 public class MainLayout extends AppLayout{
     public MainLayout() {
         addToNavbar(new DrawerToggle());
+        addToNavbar(new H2("WhatsAppDog"));
 
         final VerticalLayout menuBar = new VerticalLayout();
-        menuBar.add(new RouterLink(NavBar.TITLE, NavBar.class));
+        menuBar.add(new RouterLink(Buttons.TITLE, Buttons.class));
+
 
     }
 }
