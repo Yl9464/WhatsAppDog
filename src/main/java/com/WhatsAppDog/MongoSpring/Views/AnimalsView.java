3ff4b11de805ee574687fb3e66a2fe4f8a08ad97
Ying <86930915+Yl9464@users.noreply.github.com>
2025-11-29 09:00:04 -0500

    Dog cards rendering on DogsView

--- src/main/java/com/WhatsAppDog/MongoSpring/Views/AnimalsView.java
+++ src/main/java/com/WhatsAppDog/MongoSpring/Views/AnimalsView.java
@@ -3,12 +3,9 @@ package com.WhatsAppDog.MongoSpring.Views;
 import com.WhatsAppDog.MongoSpring.MainView;
 import com.WhatsAppDog.MongoSpring.Model.Animal;
 import com.WhatsAppDog.MongoSpring.Repository.AnimalRepo;
-import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.card.Card;
-import com.vaadin.flow.component.card.CardVariant;
-import com.vaadin.flow.component.html.*;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.server.streams.DownloadHandler;
+import com.vaadin.ui.UI;
 
 import java.util.List;
 
@@ -26,35 +23,46 @@ public class AnimalsView extends Div {
                 .set("gap", "1em");
 
         List<Animal> animals = animalRepo.findAll();
+        //create card for dog cards
 
+      //  animals.stream().filter(a -> a.name()).forEach(a -> layout.add(createAnimalCard(a));
         //create card for all animals
         for (Animal animal : animals) {
             //if animal category = dog  render cards in DogsView
+            if("Dog".equals(animal.getType())) {
+                UI.getCurrent().getNavigator().navigateTo(DogsView.ROUTE + "/" + animal.getType());
+            }
+            if("Cat".equals(animal.getType())) {
+                UI.getCurrent().getNavigator().navigateTo(CatsView.ROUTE + "/" + animal.getType());
+            }
+            else{
+                UI.getCurrent().getNavigator().navigateTo(OthersView.ROUTE + "/" + animal.getType());
+            }
             //if ....            = cat render cards in CatsView
             //else render cards in OthersView
-            layout.add(createAnimalCard(animal)); //call create method
+           // layout.add(createAnimalCard(animal)); //call create method
         }
         add(layout);
     }
 
 
-    private Component createAnimalCard(Animal animal) {
-        Card cardOutlined = new Card();
-        cardOutlined.addThemeVariants(CardVariant.LUMO_OUTLINED);
-
-        DownloadHandler imageHandler = DownloadHandler.forClassResource(
-                getClass(), animal.getImageUrl(), "Animal Pic");
-        Image image = new Image(imageHandler, "");
-        image.setWidth("100px");
-        image.setHeight("100px");
-
-        UnorderedList details = new UnorderedList(
-                new ListItem("Name: " + animal.getName()),
-                new ListItem("Category: " + animal.getType()),
-                new ListItem("Aggression: " + (animal.getAggression() ? "Yes, approach with caution" : "No"))
-        );
-        cardOutlined.add(image, details);
-
-        return cardOutlined;
-    }
+////    private Component createAnimalCard(Animal animal) {
+//        Card cardOutlined = new Card();
+//        cardOutlined.addThemeVariants(CardVariant.LUMO_OUTLINED);
+//
+//        DownloadHandler imageHandler = DownloadHandler.forClassResource(
+//                getClass(), animal.getImageUrl(), "Animal Pic");
+//        Image image = new Image(imageHandler, "");
+//        image.setWidth("100px");
+//        image.setHeight("100px");
+//
+//        UnorderedList details = new UnorderedList(
+//                new ListItem("Name: " + animal.getName()),
+//                new ListItem("Category: " + animal.getType()),
+//                new ListItem("Aggression: " + (animal.getAggression() ? "Yes, approach with caution" : "No"))
+//        );
+//        cardOutlined.add(image, details);
+//
+//        return cardOutlined;
+//    }
 }
--- src/main/java/com/WhatsAppDog/MongoSpring/Views/DogsView.java
+++ src/main/java/com/WhatsAppDog/MongoSpring/Views/DogsView.java
@@ -1,17 +1,65 @@
 package com.WhatsAppDog.MongoSpring.Views;
 
 import com.WhatsAppDog.MongoSpring.MainView;
-import com.vaadin.flow.component.html.H1;
+import com.WhatsAppDog.MongoSpring.Model.Animal;
+import com.WhatsAppDog.MongoSpring.Repository.AnimalRepo;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.card.Card;
+import com.vaadin.flow.component.card.CardVariant;
+import com.vaadin.flow.component.html.*;
+import com.vaadin.flow.component.orderedlayout.FlexLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.streams.DownloadHandler;
+
+import java.util.List;
+
 
 @Route(value= DogsView.ROUTE, layout = MainView.class)
 public class DogsView extends VerticalLayout {
     public static final String ROUTE = "dogs";
    // public static final String TITLE = "Dogs";
 
-    public DogsView() {
-        H1 h1 = new H1("Dog Cards to be rendered");
-        add(h1);
+    public DogsView(AnimalRepo animalRepo) {
+        Div dogLayout = new Div();
+        dogLayout.getStyle().set("display", "grid")
+                .set("grid-template-columns", "repeat(auto-fill, minmax(190px, 1fr))")
+                .set("gap", "1em");
+
+        setSizeFull();
+        setPadding(true);
+
+        List<Animal> animals = animalRepo.findAll();
+
+        FlexLayout container = new FlexLayout();
+        container.setFlexWrap(FlexLayout.FlexWrap.WRAP);
+        container.setJustifyContentMode(JustifyContentMode.CENTER);
+        for (Animal animal : animals) {
+            if("Dog".equals(animal.getType())) {
+               container.add(createAnimalCard(animal));
+            }
+            add(container);
+        }
+
+    }
+
+    private Component createAnimalCard(Animal animal) {
+        Card cardOutlined = new Card();
+        cardOutlined.addThemeVariants(CardVariant.LUMO_OUTLINED);
+
+        DownloadHandler imageHandler = DownloadHandler.forClassResource(
+                getClass(), animal.getImageUrl(), "Animal Pic");
+        Image image = new Image(imageHandler, "");
+        image.setWidth("100px");
+        image.setHeight("100px");
+
+        UnorderedList details = new UnorderedList(
+                new ListItem("Name: " + animal.getName()),
+                new ListItem("Category: " + animal.getType()),
+                new ListItem("Aggression: " + (animal.getAggression() ? "Yes, approach with caution" : "No"))
+        );
+        cardOutlined.add(image, details);
+
+        return cardOutlined;
     }
 }
