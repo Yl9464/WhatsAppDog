f73a17462689334a5f75893cfaf2bb93c23d71b5
Ying <86930915+Yl9464@users.noreply.github.com>
2025-12-02 07:50:07 -0500

    add data to volunteer view

--- src/main/java/com/WhatsAppDog/MongoSpring/Views/Employees.java
+++ src/main/java/com/WhatsAppDog/MongoSpring/Views/Employees.java
@@ -3,8 +3,11 @@ package com.WhatsAppDog.MongoSpring.Views;
 import com.WhatsAppDog.MongoSpring.MainView;
 import com.WhatsAppDog.MongoSpring.Model.Staff;
 import com.WhatsAppDog.MongoSpring.Repository.StaffRepo;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.router.Route;
+import org.vaadin.crudui.crud.impl.GridCrud;
 
 import java.util.List;
 
@@ -13,13 +16,19 @@ import java.util.List;
 public class Employees extends Div {
     public static  final String ROUTE = "employees";
     public Employees(StaffRepo staffRepo) {
-        Div employeeLayout = new Div();
-        employeeLayout.getStyle().set("display", "grid")
-                .set("grid-template-columns", "repeat(auto-fill, minmax(190px, 1fr))")
-                .set("gap", "1em")
-                .set("margin", "2em");
-        //getAll Data
+        H1 emplTitle = new H1("Employees");
+        GridCrud<Staff> crud = new GridCrud<>(Staff.class);
+        Grid<Staff> grid = crud.getGrid(); //access internal grid
+
+
         List<Staff> staff = staffRepo.findAll();
-        add(employeeLayout);
+        add(emplTitle, grid);
+//        for (Staff staff1 : staff) {
+//            if("Volunteer".equals(staff1.getJobTitle())) {
+//                //volTitle.add(staff1.getFirstName() + " " + staff1.getLastName());
+//            }
+//            add(employeeTitle,employeeLayout);
+        //}
+
     }
 }
--- src/main/java/com/WhatsAppDog/MongoSpring/Views/Volunteers.java
+++ src/main/java/com/WhatsAppDog/MongoSpring/Views/Volunteers.java
@@ -3,23 +3,44 @@ package com.WhatsAppDog.MongoSpring.Views;
 import com.WhatsAppDog.MongoSpring.MainView;
 import com.WhatsAppDog.MongoSpring.Model.Staff;
 import com.WhatsAppDog.MongoSpring.Repository.StaffRepo;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.router.Route;
-
-import java.util.List;
+import org.vaadin.crudui.crud.impl.GridCrud;
 
 @Route(value=Volunteers.ROUTE, layout = MainView.class)
 
 public class Volunteers extends Div{
     public static  final String ROUTE = "volunteers";
-    public Volunteers(StaffRepo staffRepo) {
-        Div volLayout = new Div();
-        volLayout.getStyle().set("display", "grid")
-                .set("grid-template-columns", "repeat(auto-fill, minmax(190px, 1fr))")
-                .set("gap", "1em")
-                .set("margin", "2em");
-        //getAll Data
-        List<Staff> staff = staffRepo.findAll();
-        add(volLayout);
+    private StaffRepo staffRepo;
+
+    //TextField searchTerm = new TextField();
+    public Volunteers(StaffRepo staff) {
+        // Div volLayout = new Div();
+        H1 volTitle = new H1("Active Volunteers");
+        GridCrud<Staff> crud = new GridCrud<>(Staff.class);
+        Grid<Staff> grid = crud.getGrid(); //access internal grid
+        grid.removeAllColumns(); //remove default headers
+        //add headers
+        grid.addColumn(Staff::getFirstName).setHeader("First Name");
+        grid.addColumn(Staff::getLastName).setHeader("Last Name");
+        grid.addColumn(Staff::getEmail).setHeader("Email");
+        grid.addColumn(Staff::getJobTitle).setHeader("Position");
+
+        crud.setFindAllOperation(staff::findAll);
+        crud.setAddOperation(staff::save);
+
+//        List<Staff> staff = staffRepo.findAll();
+        //add data
+        for (int i = 1; i <= 10; i++) {
+            Staff vol = new Staff();
+            vol.setFirstName("First Name" + i);
+            vol.setLastName("LastName" + i);
+            staff.save(vol);
+        }
+        //refresh grid
+        crud.refreshGrid();
+        add(volTitle, crud);
     }
 }
