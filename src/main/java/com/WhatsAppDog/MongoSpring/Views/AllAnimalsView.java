4ea399d5527bc83c80a06dcd92e7cb2b246b6594
Ying <86930915+Yl9464@users.noreply.github.com>
2025-12-08 05:17:28 -0500

    Intake form saveing data and displaying in AllAnimalsView grid

--- src/main/java/com/WhatsAppDog/MongoSpring/Views/AllAnimalsView.java
+++ src/main/java/com/WhatsAppDog/MongoSpring/Views/AllAnimalsView.java
@@ -1,18 +1,16 @@
 package com.WhatsAppDog.MongoSpring.Views;
 
-import com.WhatsAppDog.MongoSpring.Controller.AnimalController;
+import com.WhatsAppDog.MongoSpring.Controller.AnimalIntakeController;
 import com.WhatsAppDog.MongoSpring.MainView;
-import com.WhatsAppDog.MongoSpring.Model.Animal;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
+import com.WhatsAppDog.MongoSpring.Model.AnimalIntake;
 import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.html.Span;
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.vaadin.crudui.crud.CrudListener;
 import org.vaadin.crudui.crud.impl.GridCrud;
+
+import java.util.Collection;
 
 
 @CssImport("./styles/styles.css")
@@ -20,46 +18,80 @@ import org.vaadin.crudui.crud.impl.GridCrud;
 
 public class AllAnimalsView extends VerticalLayout {
     public static final String ROUTE = "allAnimals";
+   // private  AllAnimalsView animalRepo;
+    //private  AnimalController animalController;
 
-    private  AllAnimalsView animalRepo;
-    private  AnimalController animalController;
+    private final AnimalIntakeController intakeController;
+    private final GridCrud<AnimalIntake> crud;
 
     @Autowired
-    public AllAnimalsView(AnimalController animalController){
-        this.animalController = animalController;
-        GridCrud<Animal> animalCrud = new GridCrud<>(Animal.class);
-        //Crud Op
-       animalCrud.setAddOperation(animalController::add);
-       animalCrud.setUpdateOperation(animalController::update);
-       animalCrud.setDeleteOperation(animalController::delete);
+    public AllAnimalsView(AnimalIntakeController intakeController) {
+        this.intakeController = intakeController;
 
-         Grid<Animal> animalGrid = animalCrud.getGrid();
-        animalGrid.removeAllColumns();
-        animalGrid.addColumn(Animal::getType).setHeader("Category").setSortable(true);
-        animalGrid.addColumn(Animal::getName).setHeader("Name").setSortable(true);
-        animalGrid.addColumn(Animal::getAge).setHeader("Age").setSortable(true);
-        animalGrid.addColumn(animal -> animal.isFemale() ? "Female" : "Male").setHeader("Gender").setSortable(true);
-        animalGrid.addComponentColumn(animal -> {
-            Span badge = new Span(animal.isAggressive() ? "Aggressive" : "None");
-            if(animal.isAggressive()){ badge.getElement().getStyle().set("color", "red");};
-            return badge;
-        }).setHeader("Aggression Status").setSortable(true);
+        crud = new GridCrud<>(AnimalIntake.class);
+        crud.setCrudListener(new CrudListener<AnimalIntake>() {
+            @Override
+            public Collection<AnimalIntake> findAll() {
+                return intakeController.findAll();
+            }
 
-        animalGrid.addComponentColumn(animal -> {
-            Button delete = new Button("Delete");
-            delete.addThemeVariants(ButtonVariant.LUMO_ERROR);
+            @Override
+            public AnimalIntake add(AnimalIntake animal) {
+                return intakeController.saveAnimal(animal);
+            }
 
-            delete.addClickListener(e -> {
-                animalController.delete(animal);
-                animalCrud.refreshGrid();
-                Notification.show("Deleted " + animal.getName());
-            });
-            return delete;
-        }).setHeader("Delete");
+            @Override
+            public AnimalIntake update(AnimalIntake animal) {
+                return intakeController.saveAnimal(animal);
+            }
 
+            @Override
+            public void delete(AnimalIntake animal) {
+                intakeController.delete(animal);
+            }
+        });
+        crud.getGrid().removeAllColumns();
+        crud.getGrid().addColumn(AnimalIntake::getAnimalName).setHeader("Name").setSortable(true);
+        crud.getGrid().addColumn(AnimalIntake::getSpecies).setHeader("Species").setSortable(true);
+        crud.getGrid().addColumn(AnimalIntake::getBreed).setHeader("Breed").setSortable(true);
+        crud.getGrid().addColumn(AnimalIntake::getColor).setHeader("Color").setSortable(true);
+        crud.getGrid().addColumn(AnimalIntake::getIntakeType).setHeader("Intake Type").setSortable(true);
 
-        add(animalCrud);
+        add(crud);
         setSizeFull();
     }
 
 }
+
+
+//        GridCrud<Animal> animalCrud = new GridCrud<>(Animal.class);
+//        //Crud Op
+//       animalCrud.setAddOperation(animalController::add);
+//       animalCrud.setUpdateOperation(animalController::update);
+//       animalCrud.setDeleteOperation(animalController::delete);
+//
+//        Grid<Animal> animalGrid = animalCrud.getGrid();
+//        animalGrid.removeAllColumns();
+//        animalGrid.addColumn(Animal::getType).setHeader("Category").setSortable(true);
+//        animalGrid.addColumn(Animal::getName).setHeader("Name").setSortable(true);
+//        animalGrid.addColumn(Animal::getAge).setHeader("Age").setSortable(true);
+//        animalGrid.addColumn(animal -> animal.isFemale() ? "Female" : "Male").setHeader("Gender").setSortable(true);
+//        animalGrid.addComponentColumn(animal -> {
+//            Span badge = new Span(animal.isAggressive() ? "Aggressive" : "None");
+//            if(animal.isAggressive()){ badge.getElement().getStyle().set("color", "red");};
+//            return badge;
+//        }).setHeader("Aggression Status").setSortable(true);
+//
+//        animalGrid.addComponentColumn(animal -> {
+//            Button delete = new Button("Delete");
+//            delete.addThemeVariants(ButtonVariant.LUMO_ERROR);
+//
+//            delete.addClickListener(e -> {
+//                animalController.delete(animal);
+//                animalCrud.refreshGrid();
+//                Notification.show("Deleted " + animal.getName());
+//            });
+//            return delete;
+//        }).setHeader("Delete");
+//
+
